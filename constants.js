8f36e7ce79a593c680fb610fdd2bf20fbe26f26b
Vince Ding <vince.ding@gmail.com>
2022-08-27 12:06:49 +0800

    Camera feature completed

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 // API
 // export const API = "https://pcmob6-blog-api.haris-samingan.repl.co";
 export const API = "https://pcmob6-blog-api.vinceding.repl.co";
+
 export const API_LOGIN = "/auth";
 export const API_SIGNUP = "/newuser";
 export const API_POSTS = "/posts";
@@ -12,6 +13,8 @@ export const AUTH_SCREEN = "AUTH_SCREEN";
 export const PROFILE_SCREEN = "PROFILE_SCREEN";
 export const HOME_STACK = "HOME_STACK";
 export const NOTES_STACK = "NOTES_STACK";
+export const PROFILE_STACK = "PROFILE_STACK";
+export const CAMERA_SCREEN = "CAMERA_SCREEN";
 
 export const NOTES_SCREEN = {
   Home: "NotesScreenHome",
